f970313dc71915db811ac5258cafc91a42b13f84
Kira Todorova <kira.todorova99@gmail.com>
2023-06-17 16:52:07 +0200

    ###COMMIT 4###
    Addded:
    - Comments to explain Rust
    
    Changed:
    - main.rs file in hello_world01 project
    
    Removed:
    N/A
    
    Notes:
    - Create a new project to start working with classes separated by different Rust files.

--- Assets/Scripts/KiraT/hello_world01/src/main.rs
+++ Assets/Scripts/KiraT/hello_world01/src/main.rs
@@ -1,3 +1,7 @@
+//All lines in Rust needs ";" on the ending of every line to explicitdly end the line.
+//i32 is an Integer of 32-bits.
+//f32 is a Float of 32-bits.
+
 fn main() {
     println!("Hello, world!");
     luna_cheese();
@@ -9,6 +13,10 @@ fn main() {
     
 }
 
+//Functions are created with: fn name_of_function()
+//All functions are implicit private, but can be made explicitdly public with: pub fn name_of_function(){}.
+//All functions with Data have a "{" to open and a "}" to close.
+
 fn luna_cheese() {
     println!("Adde sucks!");
     println!("Luna is eating cheese!");
@@ -19,6 +27,16 @@ fn cheese_tax() {
 
 }
 
+//Functions with a return type are create with: fn name_of_function() -> data_type.
+//The data_type stated to be returned MUST be returned.
+//The data_type that IS returned can be returned implicitdly or explicitdly. If b = "1", return type String or &str. Then b at the end of Data will be the return type. Or by explicitdly add a "3".to_string().
+
+//"{:?}", ... or simply "{}", ... is a formating denotation for the terminal for debugging purposes only used by println();
+//"{:?}" ... is a denotation to specify and overly clear for debuggin purposes, meaning it will print the exact object.
+//"{:x}" or "{:X}" ... is a denotation for Hexa-decimal objects, so if small x in denotation, then small x in its counter part and vice-versa.
+
+//A variable is implicitdly immutable/constant, but can be explicitdly mutable with: let mut name_of_object : data_type = data;
+
 fn adde_age() -> String {
     let a : String = "3".to_string(); 
 
@@ -27,6 +45,17 @@ fn adde_age() -> String {
     a
 
 }
+
+
+//To create an object with a specific data_type: let name_of_object : data_type = data.
+//i32 is a data_type of an integer of 32-bits.
+//i64 is a data_type of an integer of 64-bits.
+//i128 is a data_type of an integer of 128-bits.
+
+//i in i32 means it's signed, which means it has a chance to overflow and it can hold negative numbers.
+
+//.to_owned() is a function to create an ownership of a data_type.
+//An unowned data_type is only borrowable from another data_type and/or is either dead or out-of-scope with: &.
 
 fn luna_age() -> i32 {
     let a : i32 = 3;
@@ -41,6 +70,10 @@ fn luna_age() -> i32 {
 
 
 }
+
+//To create a function that takes the return type of another function, which the new function has a return ype of i32, it must need to create a new object: let object_name : data_ytpe = self::function_name;
+//.parse() is a function used to parse a String-Integer like "3" to any Integer data_type., Cannot be done if it is not a number.
+//.unwrap() is a function to skip Error-Handling and it Errors occurs, then the program panics and crashes and quits it.
 
 fn kira_age() -> i32 {
 
