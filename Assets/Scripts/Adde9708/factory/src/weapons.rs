aa6c66104b17ca5414714957bcb6bed5ae3149c5
adde9708 <adde.edvin@gmail.com>
2023-06-24 21:21:50 +0200

    another formatting fix

--- Assets/Scripts/Adde9708/factory/src/weapons.rs
+++ Assets/Scripts/Adde9708/factory/src/weapons.rs
@@ -1,5 +1,6 @@
 pub mod weapons {
-    /*  Pretty much the same thing as the create army function
+    /*
+        Pretty much the same thing as the create army function
         but just use two arrays containing 4 string references
     */
     pub fn create_weapons<'a>(weapon_name: [&'a str; 4], weapons: &mut [&'a str; 4]) {
